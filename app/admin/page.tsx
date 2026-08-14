20b315cacc1464f7eab10f81485e1501d0a74ba2
mathchrtn <mathieu.charton.54@gmail.com>
2026-08-14 17:18:12 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,1 +1,50 @@
-#
+export default function AdminPage() {
+  return (
+    
+      
+        
+          🔐 Administration
+        
+
+        
+          Gestion du concours Baby Charton
+        
+
+        
+          
+            Participants
+            0
+          
+
+          
+            Pronostics validés
+            0
+          
+
+          
+            Classement
+            🏆
+          
+        
+
+        
+          
+            👶 Résultat réel
+          
+
+          
+            
+              Fille
+              Garçon
+            
+
+            
+
+            
+
+            
+
+            <input
+              type="number"
+              placeholder="Taille (cm)"
+              className="p-3
